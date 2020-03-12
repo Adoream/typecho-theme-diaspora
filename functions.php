f027c3ba69a186c3cb913ad9bafe93036b14198b
Jin <moeprprpr@gmail.com>
2020-03-12 22:57:35 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -32,14 +32,8 @@ function themeConfig ($form) {
     echo Diaspora::welcome();
          
     $form->addInput(new Title('imageTitle', NULL, NULL, _t('配图及图像管理'), NULL));
-    $defaultBg = new Typecho_Widget_Helper_Form_Element_Textarea('defaultBg', NULL, NULL, _t('站点背景大图地址'), _t('在这里填入图片的URL地址, 以在网站首页显示一个背景大图。每行填写一个链接（自动换行的视为同一行），多行则随机选一张进行展示，留空则不显示。'));
-    $form->addInput($defaultBg);
     $defaultThumbnails = new Typecho_Widget_Helper_Form_Element_Textarea('defaultThumbnails', NULL, NULL, _t('默认背景图列表'), _t('在这里填入图片的URL地址, 每行填写一个链接（自动换行的视为同一行），在文章没有配置文章主图的时候，将会从这里挑选一张图片进行显示。'));
     $form->addInput($defaultThumbnails);
-    
-    $form->addInput(new Title('extendsTitle', NULL, NULL, _t('扩展功能'), NULL));
-    $GoogleAnalytics = new Typecho_Widget_Helper_Form_Element_Text('GoogleAnalytics', NULL, NULL, _t('Google 统计代码'));
-    $form->addInput($GoogleAnalytics);
     
     $form->addInput(new Title('customExtendsTitle', NULL, NULL, _t('主题自定义扩展')));
     $customHTMLInHeadTitle = new Typecho_Widget_Helper_Form_Element_Textarea('customHTMLInHeadTitle', NULL, NULL, _t('自定义 HTML 元素拓展 - 标签: head 头部 (meta 元素后)'), _t('在 head 标签头部(meta 元素后)添加你自己的 HTML 元素<br>你可以在这里拓展一些 meta 信息, 或一些其他信息。<br>某些统计代码可能要求被加入到尽可能靠前的位置, 那么你可以将其加入到这里。<br>不建议在这里添加 css'));
