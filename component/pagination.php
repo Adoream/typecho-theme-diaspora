e61c3e1d03aa9112f79b5c6b8c22d59ef11c3c3f
Jin <moeprprpr@gmail.com>
2020-08-14 13:47:49 +0800

    修复 Ajax Url 错误

--- component/pagination.php
+++ component/pagination.php
@@ -1,19 +1,19 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<?php 
+<?php
 /*
  * @Author: Jin
  * @Date: 2020-03-12 13:43:06
  * @LastEditors: Jin
- * @LastEditTime: 2020-03-12 13:58:17
- * @FilePath: /diaspora/component/pagination.php
+ * @LastEditTime: 2020-08-14 13:44:30
+ * @FilePath: /Diaspora/component/pagination.php
  */
 ?>
-<?php 
-    if (ceil($this->getTotal() / $this->parameter->pageSize) > $this->_currentPage) {
+<?php
+if (ceil($this->getTotal() / $this->parameter->pageSize) > $this->_currentPage) {
 ?>
-<div id="pager">
-    <a href="<?php $this->options->siteUrl(); ?>/page/<?php echo (int)($this->_currentPage)+1 ?>/" data-url="<?php $this->options->siteUrl(); ?>/page/<?php echo (int)($this->_currentPage)+1 ?>/" class="more">加载更多</a>
-</div>
-<?php 
-    }
+    <div id="pager">
+        <a href="<?php echo (($this->options->rewrite) ? $this->options->siteUrl : $this->options->siteUrl . 'index.php'); ?>/page/<?php echo (int)($this->_currentPage) + 1 ?>/" data-url="<?php $this->options->siteUrl(); ?>/page/<?php echo (int)($this->_currentPage) + 1 ?>/" class="more">加载更多</a>
+    </div>
+<?php
+}
 ?>
