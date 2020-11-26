92fd2fa26da9eba66e885a9a4dfceaeb5eff3c26
Jin <moeprprpr@gmail.com>
2020-11-26 20:32:57 +0800

    🐛 fix(pagination.php): fix "syntax error, unexpected '}', expecting end of file"
    fix #5

--- component/pagination.php
+++ component/pagination.php
@@ -4,25 +4,16 @@
  * @Author: Jin
  * @Date: 2020-03-12 13:43:06
  * @LastEditors: Jin
- * @LastEditTime: 2020-08-14 13:44:30
+ * @LastEditTime: 2020-11-26 20:03:01
  * @FilePath: /Diaspora/component/pagination.php
  */
 ?>
 <?php
 if (ceil($this->getTotal() / $this->parameter->pageSize) > $this->_currentPage) {
 ?>
-<<<<<<< HEAD
     <div id="pager">
         <a href="<?php echo (($this->options->rewrite) ? $this->options->siteUrl : $this->options->siteUrl . 'index.php'); ?>/page/<?php echo (int)($this->_currentPage) + 1 ?>/" data-url="<?php $this->options->siteUrl(); ?>/page/<?php echo (int)($this->_currentPage) + 1 ?>/" class="more">加载更多</a>
     </div>
 <?php
 }
 ?>
-=======
-<div id="pager">
-    <a href="<?php echo (($this->options->rewrite) ? $this->options->siteUrl : $this->options->siteUrl . 'index.php'); ?>/page/<?php echo (int)($this->_currentPage)+1 ?>/" data-url="<?php $this->options->siteUrl(); ?>/page/<?php echo (int)($this->_currentPage)+1 ?>/" class="more">加载更多</a>
-</div>
-<?php 
-    }
-?>
->>>>>>> c6bd7e7062600aeea042fcef845a650fa90c18eb
